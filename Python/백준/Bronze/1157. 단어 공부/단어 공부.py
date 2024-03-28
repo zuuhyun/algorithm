a7e2946767302d5aad2118222c8996743f4cd23b
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-03-28 15:33:24 +0900

    [Bronze I] Title: 단어 공부, Time: 216 ms, Memory: 33076 KB -BaekjoonHub

--- Python/백준/Bronze/1157. 단어 공부/단어 공부.py
+++ Python/백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -7,8 +7,16 @@ for word in words:
     else:
         words_dic[word] = 1
 
-#리스트 컴프리헨션 사용
+'''
+리스트 컴프리헨션 사용
 answer = [key for key, value in words_dic.items() if max(words_dic.values()) == value]
+'''
+
+answer = []
+max_value = max(words_dic.values())
+for key, value in words_dic.items():
+    if value == max_value:
+        answer.append(key)
 
 if len(answer) == 1:
     print(answer[0])
