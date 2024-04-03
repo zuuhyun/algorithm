13c179b270cd4e43d3c40b5f71de12eaebdfde51
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-04-03 13:52:23 +0900

    [Gold V] Title: 탑, Time: 824 ms, Memory: 136988 KB -BaekjoonHub

--- Python/백준/Gold/2493. 탑/탑.py
+++ Python/백준/Gold/2493. 탑/탑.py
@@ -1,13 +1,18 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 top = list(map(int, input().split()))
-stack = []
 answer = [0] * n
+stack = []
+
 for i in range(n):
     while stack:
-        if top[i] < stack[-1][0]:
+        if stack[-1][0] > top[i]:
             answer[i] = stack[-1][1] + 1
             break
         else:
             stack.pop()
     stack.append([top[i],i])
+
 print(*answer)
