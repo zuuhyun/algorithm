6edcf9a8ffa5354dd9ab6650d65ebf1b03aa80ef
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-04-04 16:07:17 +0900

    [Silver I] Title: 추월, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/2002. 추월/추월.py
+++ Python/백준/Silver/2002. 추월/추월.py
@@ -1,18 +1,15 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-incar = {}
-out = []
-answer = 0
-
-for i in range(n):
-    incar[input()] = i
-
+car = [input() for _ in range(n)]
+cnt = 0
 for _ in range(n):
-    out.append(input())
+    out = input()
+    if car[0] != out:
+        cnt += 1
+        car.remove(out)
+    else:
+        car.pop(0)
 
-for i in range(n-1):
-    for j in range(i+1,n):
-        if incar[out[i]] > incar[out[j]]:
-            answer += 1
-            break
-
-print(answer)
+print(cnt)
