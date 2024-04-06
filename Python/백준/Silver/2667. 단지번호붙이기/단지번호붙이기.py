224a1a95b13fd8af6f8c26afe9e96a52ebe3661a
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-04-07 00:29:05 +0900

    [Silver I] Title: 단지번호붙이기, Time: 60 ms, Memory: 34192 KB -BaekjoonHub

--- Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,40 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-
-def bfs(graph, x, y):
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+def bfs(i,j):
     queue = deque()
-    queue.append((x,y))
-    graph[x][y] = 0
-    count = 1
-
+    queue.append((i,j))
+    graph[i][j] = 0
+    cnt = 1
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= len(graph) or ny < 0 or ny >= len(graph):
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
                 continue
+
             if graph[nx][ny] == 1:
-                queue.append((nx,ny))
                 graph[nx][ny] = 0
-                count += 1
-    return count
+                queue.append((nx,ny))
+                cnt += 1
+
+    return cnt
+
 
 n = int(input())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int, input())))
-
-cnt = []
+graph = [list(map(int, input().strip())) for _ in range(n)]
+answer = []
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 1:
-            cnt.append(bfs(graph, i, j))
+            answer.append(bfs(i,j))
 
-cnt.sort()
-print(len(cnt))
-for i in range(len(cnt)):
-    print(cnt[i])
+answer.sort()
+print(len(answer))
+for ans in answer:
+    print(ans)
