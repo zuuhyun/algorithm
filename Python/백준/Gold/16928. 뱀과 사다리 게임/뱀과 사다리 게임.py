2a8b42a77e180bc5ed38219ac9c45a9039f5e1bd
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-04-08 13:57:12 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 34088 KB -BaekjoonHub

---
+++ Python/백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,47 @@
+'''
+사다리의 개수 와 뱀의 개수 딕셔너리에 저장
+주사위를 굴리면서 주사위 굴린 횟수 저장
+'''
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n,m = map(int, input().split())
+ladders, snakes = {},{}
+
+def bfs():
+    board = [False] * 101
+    dist = [0] * 101
+
+    queue = deque()
+    queue.append(1)
+
+    while queue:
+        current = queue.popleft()
+
+        for i in range(1,7):
+            next_cell = current + i
+            if next_cell == 100:
+                return dist[current] + 1
+
+            if next_cell in ladders:
+                next_cell = ladders[next_cell]
+            elif next_cell in snakes:
+                next_cell = snakes[next_cell]
+
+            if next_cell <= 100 and not board[next_cell]:
+                board[next_cell] = True
+                dist[next_cell] = dist[current] + 1
+                queue.append(next_cell)
+
+
+for _ in range(n):
+    ladder, move = map(int, input().split())
+    ladders[ladder] = move
+
+for _ in range(m):
+    snake, move = map(int, input().split())
+    snakes[snake] = move
+
+print(bfs())
