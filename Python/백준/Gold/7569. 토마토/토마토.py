bec1710d7bf1fd42da98d854f658a508410af1cc
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-04-08 16:09:53 +0900

    [Gold V] Title: 토마토, Time: 1984 ms, Memory: 48516 KB -BaekjoonHub

--- Python/백준/Gold/7569. 토마토/토마토.py
+++ Python/백준/Gold/7569. 토마토/토마토.py
@@ -5,21 +5,22 @@ input = sys.stdin.readline
 M, N, H = map(int, input().split())
 graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
-dx = [0, 0, 1, -1, 0, 0]
-dy = [1, -1, 0, 0, 0, 0]
+dx = [1, -1, 0, 0, 0, 0]
+dy = [0, 0, 1, -1, 0, 0]
 dz = [0, 0, 0, 0, 1, -1]
 queue = deque()
 
 
-def BFS():
+def bfs():
     while queue:
         z, x, y = queue.popleft()
         for i in range(6):
             nx, ny, nz = x + dx[i], y + dy[i], z + dz[i]
-            if 0 <= nx < N and 0 <= ny < M and 0 <= nz < H:
-                if graph[nz][nx][ny] == 0:
-                    graph[nz][nx][ny] = graph[z][x][y]+1
-                    queue.append((nz, nx, ny))
+            if nx < 0 or nx >= N or ny < 0 or ny >= M or nz < 0 or nz >= H:
+                continue
+            if graph[nz][nx][ny] == 0:
+                graph[nz][nx][ny] = graph[z][x][y]+1
+                queue.append((nz, nx, ny))
 
 
 # 탐색은 입력의 반대순으로
@@ -28,18 +29,19 @@ for i in range(H):
         for k in range(M):
             if graph[i][j][k] == 1:
                 queue.append((i, j, k))
-BFS()
+bfs()
 
-cannot_complete = False
+all_ripe = True
 day = 0
 for i in range(H):
     for j in range(N):
         for k in range(M):
             if graph[i][j][k] == 0:
-                cannot_complete = True
+                all_ripe = False
+                break
             day = max(day, graph[i][j][k])
 
-if cannot_complete:
-    print(-1)
-else:
+if all_ripe:
     print(day-1)
+else:
+    print(-1)
