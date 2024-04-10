dbb40845ba037d8c91580dc60b051c9692cc81c7
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-04-10 19:59:23 +0900

    [Silver II] Title: 쇠막대기, Time: 64 ms, Memory: 31552 KB -BaekjoonHub

---
+++ Python/백준/Silver/10799. 쇠막대기/쇠막대기.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+arr = input().strip()
+stack = []
+cnt = 0
+for i in range(len(arr)):
+    if arr[i] == '(':
+        stack.append('(')
+    else:
+        if arr[i-1] == '(':
+            stack.pop()
+            cnt += len(stack)
+        else:
+            stack.pop()
+            cnt += 1
+print(cnt)
