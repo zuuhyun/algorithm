f75847272dd6912445634ba2ca40872992525172
Juhyun Seo <164435525+zuuhyun@users.noreply.github.com>
2024-09-08 15:07:26 +0900

    [Silver I] Title: 단지번호붙이기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ Python/백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,40 +1,41 @@
-from collections import deque
+#2667
 import sys
 input = sys.stdin.readline
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
-def bfs(i,j):
-    queue = deque()
-    queue.append((i,j))
-    graph[i][j] = 0
+
+def bfs(a,b):
+    queue = [(a,b)]
     cnt = 1
+    visitied[a][b] = True
     while queue:
-        x, y = queue.popleft()
+        x, y = queue.pop(0)
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            if nx < 0 or nx >= N or ny < 0 or ny >= N:
                 continue
 
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = 0
-                queue.append((nx,ny))
+            if not visitied[nx][ny] and graph[nx][ny] == 1:
                 cnt += 1
+                visitied[nx][ny] = True
+                queue.append((nx,ny))
 
     return cnt
 
+N = int(input())
+graph = [list(map(int, input().strip())) for _ in range(N)]
+visitied = [[False] * N for _ in range(N)]
 
-n = int(input())
-graph = [list(map(int, input().strip())) for _ in range(n)]
-answer = []
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            answer.append(bfs(i,j))
+res = []
+for i in range(N):
+    for j in range(N):
+        if not visitied[i][j] and graph[i][j]:
+            res.append(bfs(i,j))
 
-answer.sort()
-print(len(answer))
-for ans in answer:
-    print(ans)
+res.sort()
+print(len(res))
+for num in res:
+    print(num)
